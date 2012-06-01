07b32be6092bfe5114b2c8e4398472ea43b36e54
Thalia <thalia@thatha.(none)>
2012-05-31 23:56:24 -0300

    modifications on contacts.c due to new file contacts.h

--- tp4_ThaliaCampolina/contact.c
+++ tp4_ThaliaCampolina/contact.c
@@ -8,29 +8,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "contact.h"
 
-//cellphone contacts struct. 
-typedef struct contact {
-    char* name_;
-    int number_;
-}Contact;
-
-//creates a new contact
 Contact* CreateNewContact (){
     Contact* contact=(Contact*) malloc(sizeof(Contact));
     contact->name_=calloc(15,sizeof(char));
     return contact;
 }
-//insert name in contact struct
+
 void* InsertNameInContact(Contact* contact, char* name, int number){
     strcpy(contact->name_, name);
     contact->number_=number;
 }
-//free contact
+
 void* FreeContact(Contact* contact){
     free(contact->name_);
 }
-//returns 1 if contact1 is minor than contact2
+
 int IsMinorThen(Contact* contact1, Contact* contact2){
     char* name1=contact1->name_; 
     char* name2=contact2->name_; 
@@ -39,6 +33,3 @@ int IsMinorThen(Contact* contact1, Contact* contact2){
     }
     return 0;
 }
-
-
-
--- tp4_ThaliaCampolina/main.c
+++ tp4_ThaliaCampolina/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 void ReadFromFile(FILE* input){
     FILE* output;
     float ncontatos;
