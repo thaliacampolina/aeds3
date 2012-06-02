7913a4197e84e7335ae740f78df4de8d3e593f4a
Thalia <thalia@thatha.(none)>
2012-06-02 02:29:53 -0300

    adding heapsort files!

---
+++ tp4_ThaliaCampolina/heapsort.c
@@ -0,0 +1,43 @@
+////////////////
+// heapsort   //
+///////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "heapsort.h"
+
+void HeapSort(int vector[], int n) {
+   int i = n/2;
+   int pai, filho;
+   int t;
+ 
+   for (;;){
+      if (i > 0){
+          i--;
+          t = vector[i];
+      } else {
+          n--;
+          if (n == 0)
+             return;
+          t = vector[n];
+          vector[n] = vector[0];
+      }
+ 
+      pai = i;
+      filho = i*2 + 1;
+ 
+      while (filho < n) {
+          if ((filho + 1 < n)  &&  (vector[filho + 1] > vector[filho]))
+              filho++;
+          if (vector[filho] > t){
+             vector[pai] = vector[filho];
+             pai = filho;
+             filho = pai*2 + 1;
+          }
+          else
+             break;
+      }
+      vector[pai] = t;
+   }
+}
+
---
+++ tp4_ThaliaCampolina/heapsort.h
@@ -0,0 +1,2 @@
+
+void HeapSort(int[], int);
